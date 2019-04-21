bb6c94bffd50e293ad84586b04e0fcfbdbbf85d5
Zhiting Zhu <zhitingz@cs.utexas.edu>
2019-04-21 16:49:05 -0500

    implementing transaction similar to c++ binding; need to test it

--- pmdk-pool/src/pmemobj/Transaction.rs
+++ pmdk-pool/src/pmemobj/Transaction.rs
@@ -2,143 +2,111 @@
 // Copyright © 2017 The developers of dpdk. See the COPYRIGHT file in the top-level directory of this distribution and at https://raw.githubusercontent.com/lemonrock/dpdk/master/COPYRIGHT.
 
 /// Represents a transaction.
-#[derive(Debug, Default, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[derive(Debug, Default, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct Transaction;
 
+#[derive(Copy, Clone)]
+#[repr(u32)]
+pub enum TxStage {
+    None = pobj_tx_stage_TX_STAGE_NONE,
+    Work = pobj_tx_stage_TX_STAGE_WORK,
+    OnCommit = pobj_tx_stage_TX_STAGE_ONCOMMIT,
+    OnAbort = pobj_tx_stage_TX_STAGE_ONABORT,
+    Finally = pobj_tx_stage_TX_STAGE_FINALLY,
+}
+
+pub fn txstage_from_u32(n: u32) -> Option<TxStage> {
+    if n >= pobj_tx_stage_TX_STAGE_NONE && n < pobj_tx_stage_MAX_TX_STAGE {
+        Some(unsafe { ::std::mem::transmute(n) })
+    } else {
+        None
+    }
+}
+
+impl Drop for Transaction {
+    fn drop(&mut self) {
+        unsafe {
+            if pmemobj_tx_stage() == pobj_tx_stage_TX_STAGE_WORK {
+                pmemobj_tx_abort(ECANCELED)
+            }
+            pmemobj_tx_end();
+        }
+    }
+}
+
+quick_error! {
+    #[derive(Debug)]
+    pub enum TxError {
+        FailToStart {
+            description("Fail to start transaction")
+        }
+        WrongStage {
+            description("Wrong stage, should be in work")
+        }
+        Aborted {
+            description("Transaction aborted")
+        }
+        EndPrematurely {
+            description("Transaction ended prematurely")
+        }
+        Other(desc: String)
+    }
+}
+
 impl Transaction {
-    /// This logic currently does not work properly because Rust does not understand the implications of `setjmp` / `longjmp`.
-    /// Please note that` work` may not ever be called - in which case, the next logic called is `on_abort`.
-    #[inline(always)]
-    pub fn transaction<
-        Committed: Sized,
-        Aborted: Sized,
-        W: Fn(Transaction) -> c_int,
-        C: Fn() -> Committed,
-        A: Fn() -> Aborted,
-    >(
-        pop: *mut PMEMobjpool,
-        work: W,
-        on_commit: C,
-        on_abort: A,
-    ) -> Result<Committed, Aborted> {
-        // Must be used as a function, to prevent the volatile restrictions of setjmp leaking out
-        #[inline(never)]
-        unsafe fn internal<
-            Committed: Sized,
-            Aborted: Sized,
-            W: Fn(Transaction) -> c_int,
-            C: Fn() -> Committed,
-            A: Fn() -> Aborted,
-        >(
-            pop: *mut PMEMobjpool,
-            work: W,
-            on_commit: C,
-            on_abort: A,
-            panic_payload: &mut Option<Box<Any + Send + 'static>>,
-            function_result: &mut Option<Result<Committed, Aborted>>,
-        ) {
-            let tx_set_jmp_environment = zeroed();
-            {
-                // setjmp returns a non-zero value if returning from longjmp()
-                if setjmp(tx_set_jmp_environment) == 0 {
-                    set_error_number_if_necessary(pmemobj_tx_begin(
-                        pop,
-                        tx_set_jmp_environment,
-                        pobj_tx_param_TX_PARAM_NONE,
-                        pobj_tx_param_TX_PARAM_NONE,
-                    ));
-                } else {
-                    set_error_number_if_necessary(pmemobj_tx_errno());
-                }
-
-                let mut stage;
-                while {
-                    stage = pmemobj_tx_stage();
-                    stage != pobj_tx_stage_TX_STAGE_NONE
-                } {
-                    match stage {
-                        pobj_tx_stage_TX_STAGE_WORK => {
-                            let panic_os_error_nummber: c_int = E::ENOTSUP;
-
-                            match catch_unwind(AssertUnwindSafe(|| work(Transaction))) {
-                                Ok(some_os_error_number_for_transaction_abort) => {
-                                    if likely(some_os_error_number_for_transaction_abort == 0) {
-                                        pmemobj_tx_commit();
-                                    } else {
-                                        pmemobj_tx_abort(panic_os_error_nummber);
-                                    }
-                                }
-                                Err(payload) => {
-                                    pmemobj_tx_abort(panic_os_error_nummber);
-                                    *panic_payload = Some(payload);
-                                }
-                            };
-
-                            pmemobj_tx_process();
-                        }
-
-                        pobj_tx_stage_TX_STAGE_ONCOMMIT => {
-                            match catch_unwind(AssertUnwindSafe(|| on_commit())) {
-                                Ok(result) => *function_result = Some(Ok(result)),
-
-                                Err(payload) => {
-                                    if panic_payload.is_none() {
-                                        *panic_payload = Some(payload)
-                                    }
-                                }
-                            };
-
-                            pmemobj_tx_process();
-                        }
-
-                        pobj_tx_stage_TX_STAGE_ONABORT => {
-                            match catch_unwind(AssertUnwindSafe(|| on_abort())) {
-                                Ok(result) => *function_result = Some(Err(result)),
-
-                                Err(payload) => {
-                                    if panic_payload.is_none() {
-                                        *panic_payload = Some(payload)
-                                    }
-                                }
-                            };
-
-                            pmemobj_tx_process();
-                        }
-
-                        pobj_tx_stage_TX_STAGE_FINALLY => {
-                            pmemobj_tx_process();
-                        }
-
-                        _ => {
-                            pmemobj_tx_process();
-                        }
-                    }
-                }
-
-                pmemobj_tx_end();
-                set_error_number_if_necessary(pmemobj_tx_end());
+    pub fn new(pool: &mut ObjectPool) -> Result<(), TxError> {
+        unsafe {
+            if pmemobj_tx_begin(pool.0, ::std::ptr::null_mut(), pobj_tx_param_TX_PARAM_NONE) != 0 {
+                return Err(TxError::FailToStart);
             }
         }
+        Ok(())
+    }
 
-        let mut panic_payload = None;
-        let mut function_result = None;
-
-        unsafe {
-            internal(
-                pop,
-                work,
-                on_commit,
-                on_abort,
-                &mut panic_payload,
-                &mut function_result,
-            )
-        };
-
-        if let Some(payload) = panic_payload {
-            resume_unwind(payload);
+    pub fn run<F, R>(pool: &mut ObjectPool, f: F) -> Result<R, TxError>
+    where
+        F: FnOnce() -> R,
+    {
+        if unsafe { pmemobj_tx_begin(pool.0, ::std::ptr::null_mut(), pobj_tx_param_TX_PARAM_NONE) }
+            != 0
+        {
+            return Err(TxError::FailToStart);
         }
 
-        function_result.unwrap()
+        let res = f();
+        unsafe {
+            let stage = pmemobj_tx_stage();
+            if stage == pobj_tx_stage_TX_STAGE_WORK {
+                pmemobj_tx_commit();
+            } else if stage == pobj_tx_stage_TX_STAGE_ONABORT {
+                pmemobj_tx_end();
+                return Err(TxError::Aborted);
+            } else if stage == pobj_tx_stage_TX_STAGE_NONE {
+                return Err(TxError::EndPrematurely);
+            }
+            pmemobj_tx_end();
+        }
+        return Ok(res);
+    }
+
+    pub fn abort(err: i32) -> Result<(), TxError> {
+        unsafe {
+            if pmemobj_tx_stage() != pobj_tx_stage_TX_STAGE_WORK {
+                return Err(TxError::WrongStage);
+            }
+            pmemobj_tx_abort(err);
+        }
+        Ok(())
+    }
+
+    pub fn commit() -> Result<(), TxError> {
+        unsafe {
+            if (pmemobj_tx_stage() != pobj_tx_stage_TX_STAGE_WORK) {
+                return Err(TxError::WrongStage);
+            }
+            pmemobj_tx_commit();
+        }
+        Ok(())
     }
 }
 
